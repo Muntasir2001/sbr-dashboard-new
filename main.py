8060c49e4f64e94a69745dea0385e616fd417544
Muntasir <muntasirenamalhuda@gmail.com>
2021-10-16 16:12:21 +0100

    made a div of appropriate size

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import threading
+
+import eel
+
+eel.init('web')
+
+@eel.expose
+def demo(a, b):
+   # print(a, b, a + b)
+
+   return a + b
+
+
+@eel.expose
+def loop(a, b):
+   while a < b:
+      a += 1 
+
+def test_data():   
+   for i in range(1000):
+      eel.sendData(i)
+
+      time.sleep(0.5)
+
+x = threading.Thread(target=test_data)
+x.start()
+# test_data()
+   
+
+eel.start('index.html')
---
+++ rpm.py
@@ -0,0 +1,5 @@
+import time
+
+def rpm():
+   for i in range(1000):
+      pass
