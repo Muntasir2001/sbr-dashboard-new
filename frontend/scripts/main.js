69c79fba7ebc70922c636cd721e98e28522d651b
Muntasir <muntasirenamalhuda@gmail.com>
2021-12-27 11:19:48 +0000

    cleaned main.js and put the functions in separate js files

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,187 +1,27 @@
-const circle1 = document.querySelector('.circle-1'); // #198C19
-const circle2 = document.querySelector('.circle-2'); // #198C19
-const circle3 = document.querySelector('.circle-3'); // #198C19
-const circle4 = document.querySelector('.circle-4'); // #198C19
-const circle5 = document.querySelector('.circle-5'); // #198C19
-const circle6 = document.querySelector('.circle-6'); // FF0000
-const circle7 = document.querySelector('.circle-7'); // FF0000
-const circle8 = document.querySelector('.circle-8'); // FF0000
-const circle9 = document.querySelector('.circle-9'); // FF0000
-const circle10 = document.querySelector('.circle-10'); // FF0000
-const circle11 = document.querySelector('.circle-11'); // 0000FF
-const circle12 = document.querySelector('.circle-12'); // 0000FF
-const circle13 = document.querySelector('.circle-13'); // 0000FF
-const circle14 = document.querySelector('.circle-14'); // 0000FF
-const circle15 = document.querySelector('.circle-15'); // 0000FF
+import {
+	circle1,
+	circle2,
+	circle3,
+	circle4,
+	circle5,
+	circle6,
+	circle7,
+	circle8,
+	circle9,
+	circle10,
+	circle11,
+	circle12,
+	circle13,
+	circle14,
+	circle15,
+} from './circles.js';
+import resetNextRPMBars from './resetNextRPMBars.js';
+// import resetRPM from './resetRPM.js';
+
 const rpm = document.querySelector('.rpm');
-
-function resetRPM() {
-	circle1.style.background = '#e5e5e5';
-	circle2.style.background = '#e5e5e5';
-	circle3.style.background = '#e5e5e5';
-	circle4.style.background = '#e5e5e5';
-	circle5.style.background = '#e5e5e5';
-	circle6.style.background = '#e5e5e5';
-	circle7.style.background = '#e5e5e5';
-	circle8.style.background = '#e5e5e5';
-	circle9.style.background = '#e5e5e5';
-	circle10.style.background = '#e5e5e5';
-	circle11.style.background = '#e5e5e5';
-	circle12.style.background = '#e5e5e5';
-}
-
-function resetNextRPMBars(currentRPMCircle) {
-	if (currentRPMCircle === 1) {
-		circle2.style.background = '#e5e5e5';
-		circle3.style.background = '#e5e5e5';
-		circle4.style.background = '#e5e5e5';
-		circle5.style.background = '#e5e5e5';
-		circle6.style.background = '#e5e5e5';
-		circle7.style.background = '#e5e5e5';
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 2) {
-		circle3.style.background = '#e5e5e5';
-		circle4.style.background = '#e5e5e5';
-		circle5.style.background = '#e5e5e5';
-		circle6.style.background = '#e5e5e5';
-		circle7.style.background = '#e5e5e5';
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 3) {
-		circle4.style.background = '#e5e5e5';
-		circle5.style.background = '#e5e5e5';
-		circle6.style.background = '#e5e5e5';
-		circle7.style.background = '#e5e5e5';
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 4) {
-		circle5.style.background = '#e5e5e5';
-		circle6.style.background = '#e5e5e5';
-		circle7.style.background = '#e5e5e5';
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 5) {
-		circle6.style.background = '#e5e5e5';
-		circle7.style.background = '#e5e5e5';
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 6) {
-		circle7.style.background = '#e5e5e5';
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 7) {
-		circle8.style.background = '#e5e5e5';
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 8) {
-		circle9.style.background = '#e5e5e5';
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 9) {
-		circle10.style.background = '#e5e5e5';
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 10) {
-		circle11.style.background = '#e5e5e5';
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 11) {
-		circle12.style.background = '#e5e5e5';
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 12) {
-		circle13.style.background = '#e5e5e5';
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 13) {
-		circle14.style.background = '#e5e5e5';
-		circle15.style.background = '#e5e5e5';
-	}
-
-	if (currentRPMCircle === 14) {
-		circle15.style.background = '#e5e5e5';
-	}
-}
 
 eel.expose(updateRPM);
 function updateRPM(rpmValue) {
-	console.log('working');
 	rpm.textContent = rpmValue;
 
 	if (rpmValue > 867) {
