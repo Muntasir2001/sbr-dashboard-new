be3f92ba12c4b86b4b61577fef3119de757786a6
Muntasir <muntasirenamalhuda@gmail.com>
2021-12-27 12:07:13 +0000

    created updatedrpmvalue function

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,11 +1,13 @@
 import updateRPMBar from './updateRPMBar.js';
+import updateRPMValue from './updateRPMValue.js';
 // import resetRPM from './resetRPM.js';
 
 const rpm = document.querySelector('.rpm');
 
 eel.expose(updateRPM);
 function updateRPM(rpmValue) {
-	rpm.textContent = rpmValue;
+	// rpm.textContent = rpmValue;
 
+	updateRPMValue(rpmValue);
 	updateRPMBar(rpmValue);
 }
